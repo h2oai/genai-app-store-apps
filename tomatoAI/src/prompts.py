b4f7d5a3d2db7f9ce62d009a902df23c5792a6ef
Laura Fink <laura.fink@h2o.ai>
2023-10-25 04:55:09 -0700

    prompts

---
+++ tomatoAI/src/prompts.py
@@ -0,0 +1,25 @@
+crop_rotation = "Can you tell me three suitable, fast-growing preceding and succeeding crops for each plant?"
+bed_planning = ("Can you help me with bed planning? Take into account optimal companion planning and nutrient supply.")
+fertilizer = "Tell me which organic fertilizer suites well for my plants. "
+pests = ("What are common pests and diseases of my plants. How can I prevent and control outbreaks without pesticides?")
+biodiversity = "How can I increase the biodiversity of my vegetable garden?"
+composting = "Tell me more about composting. How can I start with it easily and how does it work?."
+climate = ("How can I reduce the effects of climate change on my vegetable garden? "
+           "Which methods can I use to reduce damage caused by extreme heat and heavy rain?")
+gardening_ai = ("How can I further use data science, machine learning and LLMs to improve my vegetable garden? "
+                "What could be interesting insights that I could gain?")
+agriculture_ai = ("How does home gardening differ from large-scale agriculture? "
+                  "How can data science, machine learning and LLMs be of use for professional farmers? ")
+
+
+questions = {
+    "crop_rotation": crop_rotation,
+    "bed_planning": bed_planning,
+    "fertilizer": fertilizer,
+    "pests": pests,
+    "biodiversity": biodiversity,
+    "composting": composting,
+    "climate": climate,
+    "gardening_ai": gardening_ai,
+    "agriculture_ai": agriculture_ai
+}
