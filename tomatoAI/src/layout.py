401a7a2478036b8e47afaa709d3c653dcb4b070b
Laura Fink <laura.fink@h2o.ai>
2023-10-30 11:53:15 -0700

    fix

--- tomatoAI/src/layout.py
+++ tomatoAI/src/layout.py
@@ -22,7 +22,6 @@ async def landing_page_view(q: Q):
     q.page['vegetables'] = plants_card(q)
     q.page['user_questions'] = questions_card()
     q.page['footer'] = footer_card()
-    q.page['chatbot'] = chat_card()
     q.page['device-not-supported'] = device_not_supported_card()
 
 
