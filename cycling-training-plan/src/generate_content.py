43903d6ed35563d03e7dd38400025fbcc504e535
Sukanya Joshi <70615558+sukanya-joshi@users.noreply.github.com>
2024-03-05 01:34:02 -0500

    Updated generate_content.py by adding streaming and changing version to h2ogpte

--- cycling-training-plan/src/generate_content.py
+++ cycling-training-plan/src/generate_content.py
@@ -1,19 +1,10 @@
 import os
-
+import asyncio
 from h2o_wave import on, ui, Q
-from h2ogpt_client import Client
+from h2ogpte import H2OGPTE
 from loguru import logger
-
+from h2ogpte.types import ChatMessage, PartialChatMessage
 from src.wave_utils import clear_cards, long_process_dialog
-
-
-def initialize_generate_content_app(q):
-    logger.info("")
-    q.app.h2ogpt = {"address": os.getenv(
-        "H2OGPT_URL"), "api_key": os.getenv("H2OGPT_API_TOKEN")}
-    q.app.system_prompt = f"You are an export at quick and easy cycling training plan." \
-        f"Do not explain yourself, just return the text of the cycling training plan."
-
 
 def initialize_generate_content_client(q):
     logger.info("")
@@ -179,30 +170,85 @@ In response use table for week plan and make structured with markdown.
     q.client.waiting_dialog = "H2OGPT is creating your training plan!"
     await long_process_dialog(q)
 
-    q.page["training_plan"].training_plan.content = await q.run(
-        llm_query_custom, q.app.system_prompt, q.client.prompt, q.app.h2ogpt
-    )
+    q.client.chatbot_interaction = ChatBotInteraction(user_message=q.client.prompt)
+
+    # Prepare our UI-Streaming function so that it can run while the blocking LLM message interaction runs
+    update_ui = asyncio.ensure_future(stream_updates_to_ui(q))
+    await q.run(chat, q.client.chatbot_interaction)
+    await update_ui
+
+    # q.page["training_plan"].training_plan.content = await q.run(
+    #     llm_query_custom, q.app.system_prompt, q.client.prompt, q.app.h2ogpt
+    # )
     await q.page.save()
 
 
-def llm_query_custom(system_prompt, prompt, connection_details):
-    logger.info(connection_details)
-    logger.info("")
+async def stream_updates_to_ui(q: Q):
+    """
+    Update the app's UI every 1/10th of a second with values from our chatbot interaction
+    :param q: The query object stored by H2O Wave with information about the app and user behavior.
+    """
+
+    while q.client.chatbot_interaction.responding:
+        q.page["training_plan"].training_plan.content = q.client.chatbot_interaction.content_to_show
+        await q.page.save()
+        await q.sleep(0.1)
+
+    q.page["training_plan"].training_plan.content = q.client.chatbot_interaction.content_to_show
+    await q.page.save()
+
+
+def chat(chatbot_interaction):
+    """
+    Send the user's message to the LLM and save the response
+    :param chatbot_interaction: Details about the interaction between the user and the LLM
+    :param chat_session_id: Chat session for these messages
+    """
+
+    def stream_response(message):
+        """
+        This function is called by the blocking H2OGPTE function periodically for updating the UI
+        :param message: response from the LLM, this is either a partial or completed response
+        """
+        chatbot_interaction.update_response(message)
+
     try:
-        logger.debug(system_prompt)
-        logger.debug(prompt)
+        client = H2OGPTE(address=os.getenv("H2OGPTE_URL"), api_key=os.getenv("H2OGPTE_API_TOKEN"))
 
-        client = Client(
-            connection_details["address"], h2ogpt_key=connection_details["api_key"])
+        collection_id = client.create_collection("temp", "")
+        chat_session_id = client.create_chat_session(collection_id)
 
-        text_completion = client.text_completion.create(
-            system_prompt=system_prompt,
-        )
-        response = text_completion.complete_sync(prompt)
+        with client.connect(chat_session_id) as session:
+            session.query(
+                system_prompt = f"You are an export at quick and easy cycling training plan." \
+        f"Do not explain yourself, just return the text of the cycling training plan.",
+                message=chatbot_interaction.user_message,
+                timeout=60,
+                rag_config={"rag_type": "llm_only"},
+                callback=stream_response,
+            )
 
-        logger.debug(response.strip())
-        return response.strip()
+        client.delete_collections([collection_id])
+        client.delete_chat_sessions([chat_session_id])
 
     except Exception as e:
         logger.error(e)
-        return ""
+        return f""
+
+
+class ChatBotInteraction:
+    def __init__(self, user_message) -> None:
+        self.user_message = user_message
+        self.responding = True
+
+        self.llm_response = ""
+        self.content_to_show = "🟡"
+
+    def update_response(self, message):
+        if isinstance(message, ChatMessage):
+            self.content_to_show = message.content
+            self.responding = False
+        elif isinstance(message, PartialChatMessage):
+            if message.content != "#### LLM Only (no RAG):\n":
+                self.llm_response += message.content
+                self.content_to_show = self.llm_response + " 🟡"
